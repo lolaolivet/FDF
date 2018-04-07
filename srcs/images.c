e4ffc1a63f12104082aaa8c90c75fb4523be5b8c
Lola OLIVET <lolivet@student.42.fr>
2018-04-07 19:18:26 +0200

    YAAAAAY

--- srcs/images.c
+++ srcs/images.c
@@ -6,7 +6,7 @@
 /*   By: lolivet <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/10 15:24:53 by lolivet           #+#    #+#             */
-/*   Updated: 2018/03/29 16:40:19 by lolivet          ###   ########.fr       */
+/*   Updated: 2018/04/07 19:10:12 by lolivet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,9 +42,9 @@ void	fill_pixel(t_fdf *d, int x, int y, int color)
 	unsigned int	green;
 	unsigned int	blue;
 
-	red = (color & 0x00ff0000) >> 16;
-	green = (color & 0x0000ff00) >> 8;
-	blue = (color & 0x000000ff);
+	red = (color & 0xff0000) >> 16;
+	green = (color & 0xff00) >> 8;
+	blue = (color & 0xff);
 	d->img_string[(y * d->img_w * 4) + (x * 4)] = blue;
 	d->img_string[(y * d->img_w * 4) + (x * 4) + 1] = green;
 	d->img_string[(y * d->img_w * 4) + (x * 4) + 2] = red;
--- srcs/parse_file.c
+++ srcs/parse_file.c
@@ -6,28 +6,17 @@
 /*   By: lolivet <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/12 14:25:20 by lolivet           #+#    #+#             */
-/*   Updated: 2018/04/06 17:35:45 by lolivet          ###   ########.fr       */
+/*   Updated: 2018/04/07 19:08:17 by lolivet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-void	check_size(t_fdf *d, int j)
+int		check_size(t_fdf *d, int j)
 {
-	/*
-	int		j;
-
-	j = 0;
-	while (j < d->nb_col && tmp[j])
-	{
-		printf("tmp: %s\nj: %d\n", tmp[j], j);
-		j++;
-	}
-	if (j != d->nb_col)
-		ft_error("Check -- Not a valid map");
-		*/
 	if (j != d->nb_col)
 		ft_error("Not a valid map");
+	return (1);
 }
 
 void	fill_points(t_fdf *d, int i, int j, int k)
@@ -46,8 +35,7 @@ void	fill_points(t_fdf *d, int i, int j, int k)
 			}
 			j++;
 		}
-		//if (j != d->nb_col)
-		//ft_error("Fill -- Not a valid map");
+		check_size(d, j);
 		j = 0;
 		i++;
 	}
@@ -59,27 +47,26 @@ void	fill_position(t_fdf *d, char **p, int i, int j)
 
 	if (!(d->pos = (int **)ft_memalloc(sizeof(int *) * d->nb_line)))
 		ft_error(strerror(errno));
-	while (i < d->nb_line)
+	while (++i < d->nb_line)
 	{
 		if (!(d->pos[i] = (int *)ft_memalloc(sizeof(int) * d->nb_col)))
 			ft_error(strerror(errno));
 		tmp = ft_strsplit(p[i], ' ');
-		while (j < d->nb_col && tmp[j])
+		ft_strdel(&(p[i]));
+		while (++j < d->nb_col && tmp[j])
 		{
 			d->pos[i][j] = ft_atoi(tmp[j]);
 			ft_strdel(&(tmp[j]));
 			if (d->pos[i][j] != 0)
 				(d->size_p)++;
-			j++;
 		}
-		ft_strdel(&(p[i]));
 		free(tmp);
-		j = 0;
-		i++;
+		check_size(d, j);
+		j = -1;
 	}
+	free(p);
 	if (d->size_p)
 		fill_points(d, 0, 0, 0);
-	free(p);
 }
 
 void	get_nb_line(t_fdf *d, char *filename)
@@ -111,7 +98,7 @@ void	parse_file(t_fdf *d, char *filename)
 	char	**tmp;
 
 	get_nb_line(d, filename);
-	if (!(line = (char **)ft_memalloc(sizeof(char *) * d->nb_line + 1)))
+	if (!(line = (char **)ft_memalloc(sizeof(char *) * (d->nb_line + 1))))
 		ft_error(strerror(errno));
 	if ((fd = open(filename, O_RDONLY)) < 0)
 		ft_error(strerror(errno));
@@ -121,14 +108,12 @@ void	parse_file(t_fdf *d, char *filename)
 		tmp = ft_strsplit(line[k++], ' ');
 		i = 0;
 		while (tmp[i])
-		{
 			ft_strdel(&(tmp[i++]));
-		}
 		d->nb_col = i > d->nb_col ? i : d->nb_col;
+		free(tmp);
 	}
-	free(tmp);
 	if (i < 0)
 		ft_error(strerror(errno));
 	close(fd);
-	fill_position(d, line, 0, 0);
+	fill_position(d, line, -1, -1);
 }
